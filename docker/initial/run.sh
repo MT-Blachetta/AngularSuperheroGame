ffaeb7496fc9ebf20d24e34798aa776761ad01b7
Manuel Hirschauer <manuel.hirschauer@valtech-mobility.com>
2020-07-26 23:14:49 +0200

    fix inital docker for tagging

--- docker/initial/run.sh
+++ docker/initial/run.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-docker run -d -p 8080:80 \
-    -v $PWD/www:/usr/share/nginx/html:ro \
-    -v $PWD/nginx:/etc/nginx:ro   \
-    --name frontend_basics_initial frontend_basics_initial
+docker run -d -p 8080:80 --name frontend_basics_initial frontend_basics_initial
